e05c84d9a2e1592c69d699f4f8357a7ded5cd1a6
Shivam <shivamv1332@gmail.com>
2023-10-02 15:27:20 +0530

    testing

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="bg-red-600">
+        <main className="bg-fuchsia-600">
           {children}
         </main>
       </body>
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>Hello This is Shivam.</h3>
+      <h3 className="text-slate-100">Hello This is Shivam.</h3>
     </main>
   )
 }
