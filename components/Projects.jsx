a8e9590dba66ed61ab2fa49aede1b85e9eac4b94
Shivam <shivamv1332@gmail.com>
2023-10-13 14:53:34 +0530

    improved github card styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -42,10 +42,10 @@ const Projects = () => {
             <div className="flex flex-wrap justify-center">
             {repos && repos.map((repo) => (
                 // <div className="w-80 bg-">{repo.name}</div>
-                <a href={repo.node.url} target="_blank" key={repo.node.id} style={{flexBasis:"33.333%"}} className="block max-w-sm p-6 m-2 h-48 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-black dark:border-gray-700 dark:hover:bg-gray-700">
+                <a href={repo.node.url} target="_blank" key={repo.node.id} style={{flexBasis:"33.333%"}} className="flex flex-col max-w-sm p-6 m-2 h-48 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-black dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-primary2"><img className="inline mr-2" width="24" height="24" src="https://img.icons8.com/windows/32/E7E5E4/repository.png" alt="repository"/>{repo.node.name}</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400 w-full max-h-20 overflow-hidden overflow-ellipsis overflow-y-clip text-left">{repo.node.description}</p>
-                    <div className="font-medium text-gray-700 dark:text-gray-400 w-80"><div style={{backgroundColor: `${repo.node.languages.edges[0].node.color}`}}className="inline-block mr-2 w-3 h-3 rounded-full"></div>{repo.node.languages.edges[0].node.name}</div>
+                    <div className="flex items-end flex-grow font-medium text-gray-700 dark:text-gray-400 w-80"><div><div style={{backgroundColor: `${repo.node.languages.edges[0].node.color}`}} className="inline-block mr-2 w-3 h-3 rounded-full"></div>{repo.node.languages.edges[0].node.name}</div></div>
                 </a>
             ))}
             {/* <object className="bg-black" type="image/svg+xml" data="https://gh-card.dev/repos/stoppieboy/promptopia.svg?fullname=&link_target=_blank"></object> */}
