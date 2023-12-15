b71f602bab639dc0ae343a6613b4185000829cf8
Shivam <shivamv1332@gmail.com>
2023-12-15 11:03:30 +0530

    fixed github api caching issue

--- app/api/get-repos/route.js
+++ app/api/get-repos/route.js
@@ -1,29 +1,10 @@
-import { ApolloClient, InMemoryCache, createHttpLink, gql } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-// export const fetchCache = 'force-no-store';
+import { gql } from "@apollo/client";
+import { getClient } from "@/lib/client";
 
 export const GET = async(req, {params}) => {
     try{
-
-        const httpLink = createHttpLink({
-            uri: "https://api.github.com/graphql",
-        });
-
-        const authLink = setContext((_, { headers }) => {
-            return {
-                headers: {
-                    ...headers,
-                    authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-                }
-            }
-        });
-        
-        const client = new ApolloClient({
-            link: authLink.concat(httpLink),
-            cache: new InMemoryCache(),
-        });
-    
+        const client = getClient()
+        console.log('here');
         const result = await client.query({
             query:gql`
             {
@@ -37,13 +18,9 @@ export const GET = async(req, {params}) => {
                                     name
                                     url
                                     description
-                                    languages(first:1){
-                                        edges{
-                                            node{
-                                                color
-                                                name
-                                            }
-                                        }
+                                    primaryLanguage{
+                                        name,
+                                        color
                                     }
                                 }
                             }
@@ -51,7 +28,11 @@ export const GET = async(req, {params}) => {
                     }
                 }
             }`,
-            fetchPolicy: 'no-cache'
+            context: {
+                fetchOptions: {
+                    next: { revalidate: 5 },
+                },
+            },
         })
 
         return new Response(JSON.stringify(result), {status: 200});
