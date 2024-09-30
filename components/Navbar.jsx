329c224f036ca3b429568640a69571f6142baf25
Shivam Verma <shivamv1332@gmail.com>
2024-09-30 19:20:15 +0530

    navbar ice frost effect

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   return (
     <div className='fixed z-10 flex w-full h-28 px-4 md:px-[34px] py-6 justify-between font-semibold text-lg items-center'>
       <div className='text-2xl font-extrabold cursor-pointer'>Shivam</div>
-      <nav className='cs-navbar font-extrabold'>
+      <nav className='cs-navbar font-extrabold ice-frost shadow-black/70'>
         <div className="overlay" id="overlay"></div>
         <ul className="flex justify-between items-center navbar w-full">
             <li className='navbar-list-items ml-[2.8px]' onMouseOver={handleMouseOver}><SmoothLink className="cs-navItem" href="#home">Home</SmoothLink></li>
